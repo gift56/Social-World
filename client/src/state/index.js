4cdaf9f8db4162a1a351399e526b011a88b427c2
gift56 <efeasiughu@gmail.com>
2022-11-25 19:12:36 +0100

    working on readux toolkit

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -33,7 +33,10 @@ export const authSlice = createSlice({
       state.posts = action.payload.posts;
     },
     setPost: (state, action) => {
-        const updatedPosts=state.posts
+      const updatedPosts = state.posts.map((post) => {
+        if (post._id === action.payload.post_id) return action.payload.post;
+        return post;
+      });
     },
   },
 });
