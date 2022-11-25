c92dd754bcff2364cbb82cbadb7244064ce7a34a
gift56 <efeasiughu@gmail.com>
2022-11-25 17:06:41 +0100

    working on some imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./scences/homePage";
 import LoginPage from "./scences/loginPage";
+import ProfilePage from "./scences/profilePage";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      
+    </div>
+  );
 };
 
 export default App;
