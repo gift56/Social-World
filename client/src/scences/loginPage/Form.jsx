c5b17e0a9e4c4ea0957cc3fd78491fce51e6f3be
gift56 <efeasiughu@gmail.com>
2022-11-26 19:50:05 +0100

    done with jsx

--- client/src/scences/loginPage/Form.jsx
+++ client/src/scences/loginPage/Form.jsx
@@ -14,7 +14,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setLogin } from "../../state";
 import Dropzone from "react-dropzone";
-import FlexBetween from "components/FlexBetween";
+import FlexBetween from "../../components/FlexBetween";
 
 // forms schemas
 const registerSchema = yup.object().shape({
