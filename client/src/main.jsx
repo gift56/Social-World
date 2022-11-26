0071116bb3dd93af175e2a716c472e4273328b55
gift56 <efeasiughu@gmail.com>
2022-11-26 10:11:13 +0100

    configuring redux persist to main.js

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,16 @@ import "./index.css";
 import authReducer from "./state";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
