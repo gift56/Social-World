2dee83a2b8278d4a1a56a7350fe3c1f049a88949
gift56 <efeasiughu@gmail.com>
2022-11-26 10:37:26 +0100

    using mui styled components

--- client/src/components/FlexBetween.jsx
+++ client/src/components/FlexBetween.jsx
@@ -4,6 +4,7 @@ import { styled } from "@mui/system";
 const FlexBetween = styled(Box)({
   display: "flex",
   justifyContent: "space-between",
+  alignItems: "center",
 });
 
 export default FlexBetween;
