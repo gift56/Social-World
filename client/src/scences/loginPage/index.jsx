315effd001c975651d32a07ca2e0c3953bc1fef9
gift56 <efeasiughu@gmail.com>
2022-11-26 13:59:07 +0100

    working on login

--- client/src/scences/loginPage/index.jsx
+++ client/src/scences/loginPage/index.jsx
@@ -1,9 +1,31 @@
-
-
 const LoginPage = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Sociopedia
+        </Typography>
+      </Box>
 
-export default LoginPage
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Socipedia, the Social Media for Sociopaths!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginPage;
