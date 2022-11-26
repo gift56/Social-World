54e53f7ad35c741ae195d9f1ceb03a2f398d0cd4
gift56 <efeasiughu@gmail.com>
2022-11-26 19:38:25 +0100

    importing some stuff

--- client/src/scences/loginPage/Form.jsx
+++ client/src/scences/loginPage/Form.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import {
-    Box,
-    Button,
-    TextField,
-    useMediaQuery,
-    Typography,
-    useTheme,
-  } from "@mui/material";
+  Box,
+  Button,
+  TextField,
+  useMediaQuery,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import { Formik } from "formik";
+import * as yup from "yup";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setLogin } from "state";
+import Dropzone from "react-dropzone";
 
 const Form = () => {
   return <div>Form</div>;
