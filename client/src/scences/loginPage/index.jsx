90229e95170348a1858e628e056f026c2cf9f4dd
gift56 <efeasiughu@gmail.com>
2022-11-26 14:03:24 +0100

    working with login form

--- client/src/scences/loginPage/index.jsx
+++ client/src/scences/loginPage/index.jsx
@@ -28,7 +28,7 @@ const LoginPage = () => {
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
           Welcome to Socialmedia, the Social Media for Sociopaths!
         </Typography>
-        {/* <Form /> */}
+        <Form />
       </Box>
     </Box>
   );
