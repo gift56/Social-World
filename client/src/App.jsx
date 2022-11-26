c97096ac2c89aebbad7892ef5556d4bd5362c159
gift56 <efeasiughu@gmail.com>
2022-11-26 10:25:17 +0100

    grabbing mode from created state

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./scences/homePage";
+import LoginPage from "./scences/loginPage";
+import ProfilePage from "./scences/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
+const App = () => {
+  const mode = useSelector((state) => state.mode);
+  
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/profile/:userId" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
