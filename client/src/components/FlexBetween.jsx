25d25dc5a5eece2b09a4197dc6a6ca5fea1140f3
gift56 <efeasiughu@gmail.com>
2022-11-26 10:35:38 +0100

    importing some stuff

--- client/src/components/FlexBetween.jsx
+++ client/src/components/FlexBetween.jsx
@@ -1,8 +1,8 @@
 import { Box } from "@mui/material";
 import { styled } from "@mui/system";
 
-const FlexBetween = () => {
-  return <div>FlexBetween</div>;
-};
+const FlexBetween = styled(Box)({
+    
+})
 
 export default FlexBetween;
