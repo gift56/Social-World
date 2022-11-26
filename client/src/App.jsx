168ec167a7f76f4333c02930dfe131a060d6a1d5
gift56 <efeasiughu@gmail.com>
2022-11-26 10:27:44 +0100

    grabbing mode from created state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,16 +11,19 @@ import { themeSettings } from "./theme";
 const App = () => {
   // getting mode from redux state
   const mode = useSelector((state) => state.mode);
+
   // Using use memo from react to create theme
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/profile/:userId" element={<ProfilePage />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 };
