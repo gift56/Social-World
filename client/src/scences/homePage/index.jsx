c91b0eaebeb18a509c8f8c8c42f062057e47716c
gift56 <efeasiughu@gmail.com>
2022-11-26 13:31:09 +0100

    working on material ui

--- client/src/scences/homePage/index.jsx
+++ client/src/scences/homePage/index.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import { Box } from "@mui/material";
+
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <Box>HomePage</Box>;
 };
 
 export default HomePage;
