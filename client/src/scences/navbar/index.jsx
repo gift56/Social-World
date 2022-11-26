e5f2d87db2e63c3b767e1a6c656bc6f338358bce
gift56 <efeasiughu@gmail.com>
2022-11-26 11:00:00 +0100

    added some state

--- client/src/scences/navbar/index.jsx
+++ client/src/scences/navbar/index.jsx
@@ -31,6 +31,7 @@ const Navbar = () => {
   const navigate = useNavigate();
   const user = useSelector((state) => state.user);
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  
   return <div>Navbar</div>;
 };
 
