8e2c36ece0439413838bf1b623563d4263ab4658
gift56 <efeasiughu@gmail.com>
2022-11-26 14:06:12 +0100

    working with login form

--- client/src/scences/loginPage/Form.jsx
+++ client/src/scences/loginPage/Form.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+    Box,
+    Button,
+    TextField,
+    useMediaQuery,
+    Typography,
+    useTheme,
+  } from "@mui/material";
 
 const Form = () => {
-  return (
-    <div>Form</div>
-  )
-}
+  return <div>Form</div>;
+};
 
-export default Form
+export default Form;
