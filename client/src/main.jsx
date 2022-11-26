63823560d403488992aff8a21dc6ec67550b3a88
gift56 <efeasiughu@gmail.com>
2022-11-26 10:10:48 +0100

    done working on readux toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App";
 import "./index.css";
 import authReducer from "./state";
 import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
