372e82f30dfa690240eaf9567d4fdcce291ab7a1
gift56 <efeasiughu@gmail.com>
2022-11-27 15:25:12 +0100

    added custormize component css

---
+++ client/src/components/UserImage.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+const UserImage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserImage
