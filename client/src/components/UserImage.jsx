ea8e1ba8ca6db3bd413dacb1896a3e0c0c6f90a5
gift56 <efeasiughu@gmail.com>
2022-11-27 15:30:15 +0100

    added custormize component css

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 
 const UserImage = ({ image, size = "60px" }) => {
-  return <Box></Box>
+  return <Box width={size} height={size}></Box>;
 };
 
 export default UserImage;
