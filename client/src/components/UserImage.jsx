9848f1192a5f5c0ba857a2720e981c2dc0dc809f
gift56 <efeasiughu@gmail.com>
2022-11-28 09:13:31 +0100

    added some updates

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -3,7 +3,13 @@ import { Box } from "@mui/material";
 const UserImage = ({ image, size = "60px" }) => {
   return (
     <Box width={size} height={size}>
-      <img src="" alt="" style={{ objectFit: "cover", borderRadius:"50%" }} />
+      <img
+        src={`http://localhost:3001/assets/${image}`}
+        alt="user"
+        style={{ objectFit: "cover", borderRadius: "50%" }}
+        width={size}
+        height={size}
+      />
     </Box>
   );
 };
