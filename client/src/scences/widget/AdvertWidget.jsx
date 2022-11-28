168f2790bdd78203410f565fed05eea9d9356393
gift56 <efeasiughu@gmail.com>
2022-11-28 10:58:28 +0100

    updated some stuff

--- client/src/scences/widget/AdvertWidget.jsx
+++ client/src/scences/widget/AdvertWidget.jsx
@@ -1,4 +1,6 @@
-
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "../../components/FlexBetween";
+import WidgetWrapper from "../../components/WidgetWrapper";
 
 const AdvertWidget = () => {
   return (
