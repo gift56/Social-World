62a7105082ac6a087fbde67d3f1918c72333327b
gift56 <efeasiughu@gmail.com>
2022-11-28 10:20:19 +0100

    working on Posts

--- client/src/scences/widget/PostsWidget.jsx
+++ client/src/scences/widget/PostsWidget.jsx
@@ -37,7 +37,37 @@ const PostsWidget = ({ userId, isProfile = false }) => {
     }
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  return <div>PostsWidget</div>;
+  return (
+    <>
+      {posts.map(
+        ({
+          _id,
+          userId,
+          firstName,
+          lastName,
+          description,
+          location,
+          picturePath,
+          userPicturePath,
+          likes,
+          comments,
+        }) => (
+          <PostWidget
+            key={_id}
+            postId={_id}
+            postUserId={userId}
+            name={`${firstName} ${lastName}`}
+            description={description}
+            location={location}
+            picturePath={picturePath}
+            userPicturePath={userPicturePath}
+            likes={likes}
+            comments={comments}
+          />
+        )
+      )}
+    </>
+  );
 };
 
 export default PostsWidget;
