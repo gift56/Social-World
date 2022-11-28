3a9d656d11c05d105cdbcb2446c1101dc7dbeccb
gift56 <efeasiughu@gmail.com>
2022-11-28 10:22:56 +0100

    working on Post

--- client/src/scences/widget/PostWidget.jsx
+++ client/src/scences/widget/PostWidget.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useState } from "react";
+import {
+  ChatBubbleOutlineOutlined,
+  FavoriteBorderOutlined,
+  FavoriteOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import Friend from "components/Friend";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "../../state";
 
 const PostWidget = () => {
-  return (
-    <div>PostWidget</div>
-  )
-}
+  return <div>PostWidget</div>;
+};
 
-export default PostWidget
+export default PostWidget;
