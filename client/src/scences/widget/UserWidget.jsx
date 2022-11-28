41b9554e78ea3da6be0c13503d49d24c8604eddf
gift56 <efeasiughu@gmail.com>
2022-11-28 10:15:18 +0100

    added some stuff

--- client/src/scences/widget/UserWidget.jsx
+++ client/src/scences/widget/UserWidget.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import {
+  ManageAccountsOutlined,
+  EditOutlined,
+  LocationOnOutlined,
+  WorkOutlineOutlined,
+} from "@mui/icons-material";
+import { Box, Typography, Divider, useTheme } from "@mui/material";
+import UserImage from "../../components/UserImage";
+import FlexBetween from "../../components/FlexBetween";
+import WidgetWrapper from "../../components/WidgetWrapper";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const UserWidget = () => {
-  return (
-    <div>UserWidget</div>
-  )
-}
+  return <div>UserWidget</div>;
+};
 
-export default UserWidget
+export default UserWidget;
