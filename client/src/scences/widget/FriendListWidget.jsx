5e176cce91ea2c5414aa8378a0c0e5c8bcc25b20
gift56 <efeasiughu@gmail.com>
2022-11-28 10:56:44 +0100

    added some updates

--- client/src/scences/widget/FriendListWidget.jsx
+++ client/src/scences/widget/FriendListWidget.jsx
@@ -10,6 +10,22 @@ const FriendListWidget = ({ userId }) => {
   const { palette } = useTheme();
   const token = useSelector((state) => state.token);
   const friends = useSelector((state) => state.user.friends);
+
+  const getFriends = async () => {
+    const response = await fetch(
+      `http://localhost:3001/users/${userId}/friends`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const data = await response.json();
+    dispatch(setFriends({ friends: data }));
+  };
+
+  useEffect(() => {
+    getFriends();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
   
   return <div>FriendListWidget</div>;
 };
