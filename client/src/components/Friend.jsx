40971750a17bf8afdba64d06853dbd46f4b86a97
gift56 <efeasiughu@gmail.com>
2022-11-28 11:29:40 +0100

    added some stuff

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -1,7 +1,7 @@
+import { useNavigate } from "react-router-dom";
 import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import { setFriends } from "../state";
 import FlexBetween from "./FlexBetween";
 import UserImage from "./UserImage";
