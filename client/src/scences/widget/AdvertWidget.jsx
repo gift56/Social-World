8986ff0b82e4ec676c0b37ca678dc87dbe20dda9
gift56 <efeasiughu@gmail.com>
2022-11-28 10:59:05 +0100

    updated some stuff

--- client/src/scences/widget/AdvertWidget.jsx
+++ client/src/scences/widget/AdvertWidget.jsx
@@ -7,8 +7,32 @@ const AdvertWidget = () => {
   const dark = palette.neutral.dark;
   const main = palette.neutral.main;
   const medium = palette.neutral.medium;
-  
-  return <div>AdvertWidget</div>;
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Sponsored
+        </Typography>
+        <Typography color={medium}>Create Ad</Typography>
+      </FlexBetween>
+      <img
+        width="100%"
+        height="auto"
+        alt="advert"
+        src="http://localhost:3001/assets/info4.jpeg"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+      <FlexBetween>
+        <Typography color={main}>MikaCosmetics</Typography>
+        <Typography color={medium}>mikacosmetics.com</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+        Your pathway to stunning and immaculate beauty and made sure your skin
+        is exfoliating skin and shining like light.
+      </Typography>
+    </WidgetWrapper>
+  );
 };
 
 export default AdvertWidget;
