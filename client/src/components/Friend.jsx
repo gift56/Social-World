15669803c06e4ced46b1835afc045c3521fcb0f4
gift56 <efeasiughu@gmail.com>
2022-11-28 10:53:25 +0100

    added some updates

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setFriends } from "../state";
+import FlexBetween from "./FlexBetween";
+import UserImage from "./UserImage";
 
-const Friend = () => {
-  return (
-    <div>Friend</div>
-  )
-}
+const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { _id } = useSelector((state) => state.user);
+  const token = useSelector((state) => state.token);
+  const friends = useSelector((state) => state.user.friends);
 
-export default Friend
+  const { palette } = useTheme();
+  const primaryLight = palette.primary.light;
+  const primaryDark = palette.primary.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  const isFriend = friends.find((friend) => friend._id === friendId);
+
+  const patchFriend = async () => {
+    const response = await fetch(
+      `http://localhost:3001/users/${_id}/${friendId}`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    dispatch(setFriends({ friends: data }));
+  };
+
+  return <div>Friend</div>;
+};
+
+export default Friend;
