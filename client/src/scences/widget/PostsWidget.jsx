8393ee586a6e2ec5733f0f7fe7dac010e7c40a33
gift56 <efeasiughu@gmail.com>
2022-11-28 10:17:55 +0100

    working on post widget

--- client/src/scences/widget/PostsWidget.jsx
+++ client/src/scences/widget/PostsWidget.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "state";
+import PostWidget from "./PostWidget";
 
 const PostsWidget = () => {
   return (
