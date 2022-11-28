770107e44dbba665fa3f13561613b2520c0e7079
gift56 <efeasiughu@gmail.com>
2022-11-28 11:33:33 +0100

    addeed some updates

--- client/src/scences/widget/FriendListWidget.jsx
+++ client/src/scences/widget/FriendListWidget.jsx
@@ -1,7 +1,7 @@
+import { useEffect } from "react";
 import { Box, Typography, useTheme } from "@mui/material";
 import Friend from "../../components/Friend";
 import WidgetWrapper from "../../components/WidgetWrapper";
-import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setFriends } from "../../state";
 
