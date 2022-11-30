145c6419bdcff95df7833564c0b734878dff67a8
gift56 <efeasiughu@gmail.com>
2022-11-30 13:20:26 +0100

    added some updates

--- client/src/scences/navbar/index.jsx
+++ client/src/scences/navbar/index.jsx
@@ -39,8 +39,7 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  // const fullName = `${user.firstName} ${user.lastName}`;
-  const fullName = `Jake Alik`;
+  const fullName = `${user.firstName} ${user.lastName}`;
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
