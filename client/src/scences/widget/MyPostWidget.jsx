7c7986ff3c4a992c48e96469cdf9dd0fb436f5b0
gift56 <efeasiughu@gmail.com>
2022-11-30 13:21:30 +0100

    importing some stuff

--- client/src/scences/widget/MyPostWidget.jsx
+++ client/src/scences/widget/MyPostWidget.jsx
@@ -1,3 +1,30 @@
+import { useState } from "react";
+import {
+  EditOutlined,
+  DeleteOutlined,
+  AttachFileOutlined,
+  GifBoxOutlined,
+  ImageOutlined,
+  MicOutlined,
+  MoreHorizOutlined,
+} from "@mui/icons-material";
+import {
+  Box,
+  Divider,
+  Typography,
+  InputBase,
+  useTheme,
+  Button,
+  IconButton,
+  useMediaQuery,
+} from "@mui/material";
+import FlexBetween from "../../components/FlexBetween";
+import Dropzone from "react-dropzone";
+import UserImage from "../../components/UserImage";
+import WidgetWrapper from "../../components/WidgetWrapper";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "state";
+
 const MyPostWidget = () => {
   return <div>MyPostWidget</div>;
 };
